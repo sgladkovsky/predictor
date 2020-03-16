8a5ea16f72671154b398f6d4f2dcc032b3dd4f2d
s.gladkovsky <s.gladkovsky@gmail.com>
2020-03-16 20:34:54 +0300

    add mysql and db migrations

---
+++ app/config.py
@@ -0,0 +1,4 @@
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Zaq123@localhost/seer'
+# 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
