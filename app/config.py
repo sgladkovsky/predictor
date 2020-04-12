fd1e131946dfdc42768712ea2b198e07d56a9695
Stas Gladkovsky <s.gladkovsky@gmail.com>
2020-04-12 20:49:42 +0300

    # flask & vue init

--- app/config.py
+++
@@ -1,4 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Zaq123@localhost/seer'
-# 'sqlite:///' + os.path.join(basedir, 'app.db')
-# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
